1b8c9fd593e9502c841c422bbb08757222154d47
mtarjan <max.tarjan@gmail.com>
2022-07-05 19:30:33 -0400

    dod assessment framework priority criteria

--- DoD-assessment-framework-20220628.R
+++ DoD-assessment-framework-20220628.R
@@ -2,13 +2,42 @@
 
 library(readxl)
 library(tidyverse)
+library(stringr)
+library(dplyr)
 
 data<-read_excel("Data/NatureServe_Assessment_Framework_Year_1_deliverable_Sep_2021.xlsx", sheet = NULL) %>% data.frame()
+
+data$Installations_NH_Count<-ifelse(is.na(data$Installations_NatureServe), 0, 1) + ifelse(is.na(data$Installations_HerpMapper), 0, 1) + ifelse(is.na(str_count(string = data$Installations_NatureServe, pattern =",")), 0, str_count(string = data$Installations_NatureServe, pattern =",")) + ifelse(is.na(data$Installations_HerpMapper), 0, 1) + ifelse(is.na(str_count(string = data$Installations_HerpMapper, pattern =",")), 0, str_count(string = data$Installations_HerpMapper, pattern =",")) ## add up number of installations plus commas (implying another installation)
+
+data$Installations_NH_Count<-subset(data, select = c(Installations_NatureServe, Installations_HerpMapper)) %>%
+  apply(MARGIN = 1, FUN = function(x) paste(x[!is.na(x)], collapse = ", ")) %>%
+  str_split(pattern = ", ") %>% 
+  lapply(unique) %>%
+  lapply(function(x){ifelse(nchar(x)==0, 0, length(x))[1]}) %>%
+  unlist()
+#subset(data, select= c(Installations_NatureServe, Installations_HerpMapper, Installations_NH_Count)) %>% head()
 
 data$new.priority<-NA
 
 ##assign priorities
-data$new.priority[which((data$Percent_MoBI_overlap > .25 & (!is.na(data$Installations_NatureServe) | !is.na(data$Installations_HerpMapper)) & data$Installation_Count_All >= 1) | (data$Percent_EOs_Overlapping > .25 & data$Installation_Count_All >= 1))] <- "I"
+data$new.priority[which(
+  (data$Percent_MoBI_overlap >= .25 
+   & data$Installation_Count_All > 1) 
+  | (data$Percent_EOs_Overlapping >= .25 
+     & data$Installation_Count_All > 1)
+  )] <- "I"
+
+data$new.priority[which(
+  is.na(data$new.priority)
+  & data$Installations_NH_Count > 1
+)] <- "II"
+
+data$new.priority[which(
+  is.na(data$new.priority) 
+  & data$Installations_NH_Count == 1
+)] <- "III"
+
+table(data$new.priority, data$Priority_NatureServe)
 
 ##create list of species for modeling in year 3
 ##add which species have been modeled in previous years
