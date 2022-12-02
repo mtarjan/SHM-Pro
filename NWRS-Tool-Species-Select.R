e10e6f6c04b3d730a63ccf3ede3adf3a4588f029
Max Tarjan <Max_Tarjan@nature.serve>
2022-12-02 13:51:08 -0800

    write out list of species for permissions

--- NWRS-Tool-Species-Select.R
+++ NWRS-Tool-Species-Select.R
@@ -23,13 +23,13 @@ models <- read_excel("C:/Users/max_tarjan/NatureServe/Conservation Science - App
 eos <- read_excel("Data/Biotics_EO_Summary.xlsx", sheet= "EO_Summary_202207")
 
 ## Get the number of current eos and all relevant subnations
-eos.conus <- eos %>% filter(!(SUBNATION_CODE %in% c("AK", "HI")) & NATION == "United States" & NUM_CURRENT_EOS > 0) %>% group_by(ELEMENT_GLOBAL_ID) %>% summarise(NUM_CURRENT_EOS = sum(NUM_CURRENT_EOS), STATES = paste0(SUBNATION_CODE, collapse = ", "))
+eos.conus <- eos %>% filter(!(SUBNATION_CODE %in% c("AK", "HI")) & NATION == "United States" & NUM_CURRENT_EOS > 0) %>% group_by(ELEMENT_GLOBAL_ID) %>% summarise(NUM_CURRENT_EOS = sum(NUM_CURRENT_EOS), EO_STATES = paste0(SUBNATION_CODE, collapse = ", "))
 
 ## Query Biotics for relevant species
 ## NEED TO FIRST CONNECT TO VPN
 con<-odbcConnect("centralbiotics", uid="biotics_report", pwd=rstudioapi::askForPassword("Password")) ##open connection to database
 
-qry <- "SELECT DISTINCT egt.element_global_id, gname.scientific_name, egt.g_primary_common_name, nc.name_category_desc, egt.rounded_g_rank, ESA_TG.D_USESA_ID
+qry <- "SELECT DISTINCT egt.element_global_id, gname.scientific_name, egt.g_primary_common_name, nc.name_category_desc, egt.rounded_g_rank, ESA_TG.D_USESA_ID, d_usesa.display_value USESA
 FROM  element_global egt
 LEFT JOIN scientific_name gname
   ON egt.gname_id = gname.scientific_name_id
@@ -39,6 +39,8 @@ LEFT JOIN element_global_rank egr
     ON egt.element_global_id = egr.element_global_id
 LEFT JOIN taxon_global esa_tg
     ON egt.element_global_id = esa_tg.element_global_id
+LEFT JOIN d_usesa
+  ON esa_tg.d_usesa_id = d_usesa.d_usesa_id
 WHERE
 /* criteria that applies to all records - active, regular and confident in US */ 
   egt.inactive_ind = 'N' 
@@ -61,7 +63,7 @@ dat <- left_join(dat, data.frame(ELEMENT_GLOBAL_ID = eos.conus$ELEMENT_GLOBAL_ID
 
 ## Subset element global ids to include those that meet each parameter (candidates)
 ## G1,g2,g3, t1, t2, t3, esa listed, or sgcn that is tracked by the network for which we have data (eos or models) - how many have eos, how many have models 
-## CONUS - no AK or HI
+## CONUS - no AK or HI (EOs only reported for CONUS)
 cand <- dat %>%
   ##esa id 39 is delisted
   ##can also include sgcns
@@ -69,3 +71,8 @@ cand <- dat %>%
   group_by(modeled, eos) %>% summarise(n = n()) %>%
   data.frame()
 cand
+
+## Get list of species for permissions
+permissions <- left_join(dat, eos.conus) %>% 
+  filter(ROUNDED_G_RANK %in% c("G1", "G2", "T1", "T2") | (!is.na(D_USESA_ID) & D_USESA_ID != 39) | sgcn) %>%
+  filter(modeled | eos)
