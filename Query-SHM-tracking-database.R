5e5a54e87c021e23256503691f1dfdddde57d786
Max Tarjan <Max_Tarjan@nature.serve>
2023-07-17 16:00:04 -0700

    additional model queries

--- Query-SHM-tracking-database.R
+++ Query-SHM-tracking-database.R
@@ -20,7 +20,7 @@ models %>% filter(project1 == "DOD0R004" & year_project1 == "three") %>% select(
 models %>% filter((project1 == "USW0R035" | project2 == "USW0R035") & year_project1 == "NWRS") %>% select(model_status) %>% table()
 
 ##search by species name
-models %>% filter(grepl(x = scientific_name, pattern = "Thomomys clusius") & project1 != "MOBI") %>% select(common_name, project1, year_project1, model_status) %>% data.frame()
+models %>% filter(grepl(x = scientific_name, pattern = "Hymenocallis henryae") & project1 != "MOBI") %>% select(common_name, project1, year_project1, model_status) %>% data.frame()
 
 ##how many models created in 2022
 models %>% filter(grepl(x = model_version...3, pattern = "_2022")) %>% select(model_status, author_organization) %>% table()
@@ -33,3 +33,7 @@ models %>% select(model_status, scientific_name) %>% unique() %>% select(model_s
 
 ##blm mobi model uploads
 models %>% filter(year_project1 == "[BLMuploads]") %>% select(model_status) %>% table()
+
+##models delivered to fws se
+models.fwsse <- models %>% filter((project1 == "USW0R035" | project2 == "USW0R035")) %>% select(element_global_id, scientific_name, common_name) %>% data.frame() %>% unique() %>% mutate(element_global_id = unlist(element_global_id))
+#write.csv(models.fwsse, "Models_delivered_FWS_SE_20230717.csv", row.names = F)
