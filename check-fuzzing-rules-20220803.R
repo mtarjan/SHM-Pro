da727595dc77a2f6ad248d41c69091a09a067f4d
Max Tarjan <Max_Tarjan@nature.serve>
2023-08-09 11:33:56 -0700

    updated folder path to onedrive

--- check-fuzzing-rules-20220803.R
+++ check-fuzzing-rules-20220803.R
@@ -3,7 +3,8 @@
 library(readxl)
 library(tidyverse)
 species.df <- read.csv("Data/FY22-model-species.csv") ##read in the species to be modeled (for permissions)
-biotics <- read_xlsx("Data/Biotics_EO_Summary.xlsx", sheet="EO_Summary_202207") ##read in fuzzing requirements by species
+# biotics <- read_xlsx("Data/Biotics_EO_Summary.xlsx", sheet="EO_Summary_202207") ##read in fuzzing requirements by species
+biotics <- read_xlsx("C:/Users/max_tarjan/OneDrive - NatureServe/MJD & Data Requests Spreadsheet/Biotics_EO_Summary.xlsx", sheet="EO_Summary_202306") ##read in fuzzing requirements by species
 species.fuzz <- left_join(species.df, subset(biotics, select = c(GNAME, ELEMENT_GLOBAL_ID, SUBNATION_CODE, NUM_EOS, NUM_DATA_SENS_EOS, EST_DATA_SENS)), by = c("ELEMENT_GLOBAL_ID" = "ELEMENT_GLOBAL_ID"))
 
 ##check which species didn't have a match in the EO data
